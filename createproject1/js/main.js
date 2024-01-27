7f3c78f7e658d12a527a7668a04c9fd6fbf17029
TLCChen <139132998+TLCChen@users.noreply.github.com>
2024-01-26 20:13:52 -0500

    1-25-24

--- createproject1/js/main.js
+++ createproject1/js/main.js
@@ -1,38 +1,10 @@
-const url1 = "https://pokeapi.co/api/v2/pokemon/2";
+import { pokemons, bosses } from "./pokemon";
 
-async function getData(URl) {
-  const response = await fetch(URl);
-  const data = await response.json();
-  console.log(data.name);
-}
-// getData(url1);
-// put in async function
-let x = 0;
-function call() {
-  while (x != 5) {
-    x = Math.floor(Math.random() * 6);
-    console.log(x);
-  }
-}
-//   getData(`https://pokeapi.co/api/v2/pokemon/${x + 1}`);
-
-function wait() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("ow");
-    }, 2000);
-  });
-}
-
-function doSomething() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      // Other things to do before completion of the promise
-      console.log("Did something");
-      // The fulfillment value of the promise
-      resolve("https://example.com/");
-    }, 2000);
-  });
-}
-
-doSomething();
+pokemons.forEach((call) =>
+  document.querySelector("#box2").insertAdjacentHTML(
+    "afterbegin",
+    `
+<img src=${call.url} alt=${call.name}>
+`
+  )
+);
